d34dacd819e50414d9e122ddbfad6f28c6685a22
tobyStupp <40300257+tobyStupp@users.noreply.github.com>
2021-11-14 21:16:24 +0200

    PRONTO

---
+++ Node_Tiyul/NodeManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4;
+
+namespace Node_Tiyul
+{
+    class NodeManager
+    {
+        public static Node<int> CreateNewNodeList(int count=12)
+        {
+            Random rnd = new Random();
+            Node<int> first = new Node<int>(rnd.Next(100) + 1);
+            Node<int> cont = first;
+            for (int i = 1; i < count; i++)
+            { 
+                cont.SetNext(new Node<int>(rnd.Next(100) + 1));
+                cont = cont.GetNext();
+            }
+            return first;
+        }
+        public static void Print (Node<int> first)
+        {
+            Console.Write(first.GetValue());
+            first = first.GetNext();
+            while (first!= null)
+            {
+                Console.Write(", {0}", first.GetValue());
+                first = first.GetNext();
+            }
+        }
+        
+    }
+}
--- Node_Tiyul/Program.cs
+++ Node_Tiyul/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unit4;
 
 namespace Node_Tiyul
 {
     class Program
     {
+        public static int Count (Node<int> p)
+        {
+            // Gets the beginning of a Linked List and returns how many  Nodes there are
+            throw new NotImplementedException();
+        }
+        public static int SumEven (Node<int> p)
+        {
+            // calculates sum of all even variables that are even in the nulls
+            throw new NotImplementedException();
+        }
+        public static int GetBiggest (Node<int> p)
+        {
+            //gets the value of the largest node in the list
+            throw new NotImplementedException();
+        }
+        public static bool IsSorted (Node<int> p)
+        {
+            // returns true if the Linked list starting at p is sorted?
+            throw new NotImplementedException();
+        }
+        public static bool Exists (Node<int> p , int x)
+        {
+            //Gets the beginning of a linked list and returns true if you found X int the list and false if not
+            throw new NotImplementedException();
+        }
         static void Main(string[] args)
         {
+            Node<int> p = NodeManager.CreateNewNodeList();
+            NodeManager.Print(p);
+            Console.WriteLine($"there are {Count(p)}");
+           Console.WriteLine ($"The biggest is {GetBiggest(p)}");
+
+
+            Console.WriteLine("input number to find");
+            int x = int.Parse(Console.ReadLine());
+            // check if lis sorted
+            if (Exists(p, x))
+                Console.WriteLine("FOUND");
+            else
+                Console.WriteLine("NOT FOUND);
+      
+
+
+
         }
     }
 }
